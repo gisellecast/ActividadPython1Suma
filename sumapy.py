6fbdbff0deab275b75f07ce56f2dd5c85666cff0
Giselle Castellanos <ada_hack149@hackacademy.mx>
2022-04-04 23:59:34 -0500

    ejerciciosuma
    Python1 Ejercicio de Suma

---
+++ sumapy.py
@@ -0,0 +1,15 @@
+#función suma py
+def suma():
+    numero1 = int(input("Numero 1: "))
+    numero2 = int(input("Numero 2: "))
+    suma = numero1 + numero2
+    print(f"La suma de {numero1} + {numero2} = {suma}")
+
+suma()
+print()
+
+def numeros_suma(x, y):
+        suma = x + y
+        print(f"La suma es {suma}")
+numeros_suma(3, 2)
+
